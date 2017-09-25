ff02810e320f37a11696ff411b25bb4116e1e99d
qwerty36963 <sparks21012@gmail.com>
2017-09-25 18:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var name = "susan"
+var name = "Susan"
 var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "susan") {console.log(" the name is correct")}
+if (name === "Susan") {console.log("the name is correct")}
 
- else {console.log(" expected the name to be susan")}
+ else {console.log("expected the name to be Susan")}
